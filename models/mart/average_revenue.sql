83d7ee83471ed68779859c97e0c23293ed7048e0
aleck.oliveira@maxihost.com.br <aleck.oliveira@maxihost.com.br>
2022-11-23 17:39:06 +0000

    customer lifetime value

--- models/mart/average_revenue.sql
+++ models/mart/average_revenue.sql
@@ -2,7 +2,6 @@ with customer_count as (
 	select 
 		date_trunc('month', "date")::date as mrr_month, 
 		count(distinct customer_id) as customers_count,
-
 		stripe_account 
 	from {{ref('historical_mrr')}} 
 	where mrr > 0
