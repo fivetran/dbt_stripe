3b975e0daa2d9b97e99fdba0329ae99a5ff24456
aleck.oliveira@maxihost.com.br <aleck.oliveira@maxihost.com.br>
2022-11-29 19:50:44 +0000

    churn rate view

---
+++ models/mart/churn.sql
@@ -0,0 +1,9 @@
+select 
+    date_trunc('month', mrr_month )::date as churn_month, 
+    customer_id,
+    name,
+    stripe_account 
+from {{ref('mrr_movements_monthly')}} mmm 
+where event_type = 'Churn'
+    and customer_id NOT IN ('cus_MVjwgFklliUF9p','cus_J8IS1IGMxzZLzR')
+order by 1 desc
