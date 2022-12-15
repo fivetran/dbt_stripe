db948625b35e63229dde1a756bd29baaeb89cb0c
Renee Li <91097070+fivetran-reneeli@users.noreply.github.com>
2022-12-15 16:56:08 +0800

    Update .buildkite/scripts/run_models.sh
    Co-authored-by: Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -19,5 +19,5 @@ dbt deps
 dbt seed --target "$db" --full-refresh
 dbt run --target "$db" --full-refresh
 dbt test --target "$db"
-dbt run --vars '{stripe__using_invoices: false, stripe__using_payment_method: false, stripe__using_subscriptions: false, stripe_timezone: "America/New_York", stripe__using_subscription_history: false, stripe__price: false}' --target "$db"
+dbt run --vars '{stripe__using_invoices: false, stripe__using_payment_method: false, stripe__using_subscriptions: false, stripe_timezone: "America/New_York", stripe__using_subscription_history: false, stripe__using_price: false}' --target "$db"
 dbt test --target "$db"
