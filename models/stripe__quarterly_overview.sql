ed8f37a7dfb570c3ff4d37d0fb8889851340ca19
fivetran-reneeli <renee.li@fivetran.com>
2023-01-23 16:46:28 -0600

    updating the quarterly model but will hold on the rest til we decide

--- models/stripe__quarterly_overview.sql
+++ models/stripe__quarterly_overview.sql
@@ -8,19 +8,19 @@ with daily_overview as (
 select
     {{ dbt.date_trunc('quarter', 'date')}} as quarter,
     source_relation,
-    sum(total_sales) as total_sales,
-    sum(total_refunds) as total_refunds,
-    sum(total_adjustments) as total_adjustments,
-    sum(total_other_transactions) as total_other_transactions,
-    sum(total_gross_transaction_amount) as total_gross_transaction_amount,
-    sum(total_net_transactions) as total_net_transactions,
-    sum(total_payout_fees) as total_payout_fees,
-    sum(total_gross_payout_amount) as total_gross_payout_amount,
-    sum(daily_net_activity) as quarterly_net_activity,
-    sum(total_sales_count) as total_sales_count,
-    sum(total_payouts_count) as total_payouts_count,
-    sum(total_adjustments_count) as total_adjustments_count,
-    sum(total_failed_charge_count) as total_failed_charge_count,
-    sum(total_failed_charge_amount) as total_failed_charge_amount
+    sum(total_daily_sales_amount) as total_daily_sales_amount,
+    sum(total_daily_refunds_amount) as total_daily_refunds_amount,
+    sum(total_daily_adjustments_amount) as total_daily_adjustments_amount,
+    sum(total_daily_other_transactions_amount) as total_daily_other_transactions_amount,
+    sum(total_daily_gross_transaction_amount) as total_daily_gross_transaction_amount,
+    sum(total_daily_net_transactions_amount) as total_daily_net_transactions_amount,
+    sum(total_daily_payout_fee_amount) as total_daily_payout_fee_amount,
+    sum(total_daily_gross_payout_amount) as total_daily_gross_payout_amount,
+    sum(daily_net_activity_amount) as daily_net_activity_amount,
+    sum(total_daily_sales_count) as total_daily_sales_count,
+    sum(total_daily_payouts_count) as total_daily_payouts_count,
+    sum(total_daily_adjustments_count) as total_daily_adjustments_count,
+    sum(total_daily_failed_charge_count) as total_daily_failed_charge_count,
+    sum(total_daily_failed_charge_amount) as total_daily_failed_charge_amount
 from daily_overview
 group by 1,2
