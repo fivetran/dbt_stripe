49f0b2478a3f0f3d4d51ae39f98fdfe7842eb426
Savage Fivetran <sarah.savage@fivetran.com>
2025-09-16 15:18:24 -0700

    add_subscription_config_to_model

--- models/intermediate/int_stripe__deduped_subscription_item.sql
+++ models/intermediate/int_stripe__deduped_subscription_item.sql
@@ -1,4 +1,5 @@
 with subscription_item as (
+{{ config(enabled=var('stripe__using_subscriptions', True)) }}    
 
     select * 
     from {{ ref('stg_stripe__subscription_item') }}
@@ -9,11 +10,11 @@ with subscription_item as (
 SUBSCRIPTION_ITEM allows for one-to-many relationships between subscriptions and plans, so we need to dedupe to the subscription_id level
 */
 
-    select
-        subscription_id,
-        source_relation,
-        min(current_period_start) as current_period_start,
-        max(current_period_end) as current_period_end
+select
+    subscription_id,
+    source_relation,
+    min(current_period_start) as current_period_start,
+    max(current_period_end) as current_period_end
         
-    from subscription_item
-    group by subscription_id, source_relation
+from subscription_item
+group by subscription_id, source_relation
