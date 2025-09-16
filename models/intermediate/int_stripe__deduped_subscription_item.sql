59f89f74422fadcf0e99251324c17db4587d6eb9
Savage Fivetran <sarah.savage@fivetran.com>
2025-09-16 15:13:37 -0700

    remove_comment_line

--- models/intermediate/int_stripe__deduped_subscription_item.sql
+++ models/intermediate/int_stripe__deduped_subscription_item.sql
@@ -6,7 +6,6 @@ with subscription_item as (
 )
 
 /*
-Newer Stripe connections will store current_period_start/end fields in SUBSCRIPTION_ITEM while older ones house these fields in SUBSCRIPTION_HISTORY -> grab both and coalesce
 SUBSCRIPTION_ITEM allows for one-to-many relationships between subscriptions and plans, so we need to dedupe to the subscription_id level
 */
 
